35209c70e1ed71f6318c2abb1c15a3dc2f8f9f27
Nagy Levente <e9ca6n@inf.elte.hu>
2020-02-23 19:21:33 +0100

    Profil ujragondolas : uj modell Profile, ami a beepitett auth_user boviti

--- ScrumHelper/users/admin.py
+++ ScrumHelper/users/admin.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 
 from .models import User
 
-admin.site.register(User)
+#admin.site.unregister(User)
+#admin.site.register(User, MyUserAdmin)
 
--- ScrumHelper/users/models.py
+++ ScrumHelper/users/models.py
@@ -1,18 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
-class User(models.Model):
-    uuid = models.IntegerField(primary_key=True, db_index=True,
-                               auto_created=True,unique=True)
-    username = models.CharField("username", max_length=255, unique=True,
-        help_text="Required. 30 characters or fewer. Letters, numbers and "
-                    "/./-/_ characters"
-        )
-    full_name = models.CharField("full name", max_length=256, blank=True)
-    email = models.EmailField("email address", max_length=255, blank=True, unique=True)
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=30, blank=True)
 
-    def getFullName(self):
-        return self.full_name
+    #def getFullName(self):
+    #    return self.full_name
 
     def __str__(self):
         return self.username
+
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
