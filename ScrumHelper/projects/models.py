b96c78d0f9536294858515d4da5bf709fa778ed5
Nagy Levente <e9ca6n@inf.elte.hu>
2020-03-02 16:55:47 +0100

    projects model kreálás

--- ScrumHelper/projects/models.py
+++ ScrumHelper/projects/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+from users.models import Profile
+
+class Project(models.Model):
+    """
+        Project object's model class.
+    """
+    id = models.IntegerField(verbose_name="project_id", name="project_id",
+                             primary_key=True, unique=True, db_index=True)
+    name = models.CharField(max_length=50, unique=True)
+    code = models.CharField(max_length=6, unique=True)
+    created_date = models.DateField()
+    project_owner = models.ForeignKey("users.Profile", on_delete=models.CASCADE)
+    documents = models.FileField()
+
