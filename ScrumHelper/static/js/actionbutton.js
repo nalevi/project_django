7076c9d5a7d12739553712bc7462466fe62e4cdb
Nagy Levente <e9ca6n@inf.elte.hu>
2020-03-21 15:26:24 +0100

    frontend kontírozás

--- ScrumHelper/static/js/actionbutton.js
+++ ScrumHelper/static/js/actionbutton.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
     var instances = M.FloatingActionButton.init(elems, {
-                                                        direction: 'left',
+                                                    direction: 'left',
                                                     });
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems, {position: 'top',});
   });
